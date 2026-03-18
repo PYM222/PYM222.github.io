a023747fa6402a14f53a72b9e1f6cd9367c2ea69
PYM222 <ahwnsvy@mju.ac.kr>
2026-03-18 19:37:53 +0900

    complete version

---
+++ sample.js
@@ -0,0 +1,33 @@
+//라이브러리(도서관)
+//import (라이브러리 이름);
+//import *(모든 것) from 'three'
+//as (별명) : three라는 라이브러리로부터 모든 기능을 가져와 쓰겠다.
+//이 라이브러리 안의 기능은 내가 THREE라고 부를게
+import * as THREE from 'three';
+
+//라이브러리 기능 가져다 쓰기.
+//장면 = 새로운 THREE.장면;
+//객체지향 -> 붕어빵 굽기
+const scene = new THREE.Scene();
+scene.background = new THREE.COLOR(0xf0f0f0);
+//()안에 매개변수
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innetHeight, 0.1, 1000);
+camera.position.set(0, 5, 10);
+camera.lookAt(0, 2, 0);
+
+window.addEventListener('pointerdown', (event)=> {
+	mouse.X = (event.clientX / window.innerWidth) * 2 -1;
+	mouse.Y = (event.clientY / window.innerHeight) * 2 +1;
+
+	raycaster.setFromCamera(mouse, camera);
+	const intersects = raycaster.intersectObject(ball);
+
+	if (intersects.length > 0){
+		isDragging = true;
+		velocity = 0;
+
+		const normal = new THREE.Vector3();
+		camera.getWorldDirection(normal);
+		dragPlane.setFromNormalAndCoplanarPoint(normal.negate(), ball.position);
+}
